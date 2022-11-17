e7c145194f675c03f79f1f57190d0d4e212f160f
Eliezer <eliezer.rodriguezr2@gmail.com>
2022-11-17 15:10:22 -0300

    performance optimization

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import PostsList from "./app/features/posts/PostsList";
 import AddPostForm from "./app/features/posts/AddPostForm";
 import SinglePostPage from "./app/features/posts/SinglePostPage";
 import EditPostForm from "./app/features/posts/EditPostForm";
+import UsersList from "./app/features/users/UsersList";
+import UserPage from "./app/features/users/UserPage";
 import Layout from "./components/Layout";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -16,6 +18,14 @@ function App() {
           <Route path=":postId" element={<SinglePostPage />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        {/* Catch all - replace with 404 component if you want */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
